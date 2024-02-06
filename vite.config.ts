6a83f6f7e00e7d345ea9edd702deeccdfd0182ab
Luiz Augusto <augustodia97@gmail.com>
2024-02-05 22:40:07 -0300

    start deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader({svgoConfig: {
-    plugins: [
-        'prefixIds',
-    ]
-}})],
-})
+  plugins: [
+    vue(),
+    svgLoader({
+      svgoConfig: {
+        plugins: ["prefixIds"],
+      },
+    }),
+  ],
+  server: {
+    https: true,
+  },
+});
